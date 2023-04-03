e687460d96f06cc4d995be6a834e8cab5cb90893
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-04-03 13:55:23 -0300

    add HTML, css, js structure. Add modules files and make changes to index.js structure

---
+++ index.js
@@ -0,0 +1,46 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { DateTime } from './modules/luxon.js';
+import Books from './modules/books.js';
+
+// navbar functionality
+const navlist = document.querySelector('.navList');
+const navadd = document.querySelector('.navAdd');
+const navcont = document.querySelector('.navCont');
+const listPage = document.querySelector('.list');
+const newBookPage = document.querySelector('.form-container');
+const contactPage = document.querySelector('.contact-info');
+
+navlist.addEventListener('click', () => {
+  listPage.classList.remove('hide');
+  newBookPage.classList.add('hide');
+  contactPage.classList.add('hide');
+});
+
+navadd.addEventListener('click', () => {
+  listPage.classList.add('hide');
+  newBookPage.classList.remove('hide');
+  contactPage.classList.add('hide');
+});
+
+navcont.addEventListener('click', () => {
+  listPage.classList.add('hide');
+  newBookPage.classList.add('hide');
+  contactPage.classList.remove('hide');
+});
+
+// Creating a instance of Books
+const awesomeBooks = new Books();
+
+// Checking Local Storage
+if (localStorage.getItem('createdBooks')) {
+  awesomeBooks.books = JSON.parse(localStorage.getItem('createdBooks'));
+  awesomeBooks.displayBooks();
+}
+
+// Add date and time
+const displayDate = () => {
+  const date = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+  document.getElementById('datetime').innerHTML = date;
+};
+
+document.addEventListener('DOMContentLoaded', displayDate());
